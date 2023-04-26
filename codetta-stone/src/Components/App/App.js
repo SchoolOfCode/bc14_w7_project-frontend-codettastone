c72159176ac22d186e61d64edb3ae4e2e8f6e72d
Jay Kirtlan <jaykirtlan@hotmail.com>
2023-04-26 13:45:05 +0100

    Nav bar begun

--- codetta-stone/src/Components/App/App.js
+++ codetta-stone/src/Components/App/App.js
@@ -1,8 +1,10 @@
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Sidebar from '../SideBar/index.js';
 
 function App() {
-  return 
+  return <> <Sidebar/> </>
     
    
   
--- codetta-stone/src/Components/SideBar/index.js
+++ codetta-stone/src/Components/SideBar/index.js
@@ -1,27 +1,22 @@
-import { useState } from 'react';
-import Alert from 'react-bootstrap/Alert';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import Alert from 'react-bootstrap/Alert';
 
-function ResponsiveExample() {
+
+
+export default function Sidebar() {
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
   return (
     <>
-      <Button variant="primary" className="d-lg-none" onClick={handleShow}>
+      <Button variant="primary" onClick={handleShow}>
         Launch
       </Button>
-
-      <Alert variant="info" className="d-none d-lg-block">
-        Resize your browser to show the responsive offcanvas toggle.
-      </Alert>
-
       <Offcanvas show={show} onHide={handleClose} responsive="lg">
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Responsive offcanvas</Offcanvas.Title>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
           <p className="mb-0">
@@ -29,8 +24,21 @@ function ResponsiveExample() {
           </p>
         </Offcanvas.Body>
       </Offcanvas>
+      <Button variant="primary" className="d-lg-none" onClick={handleShow}>
+        Launch
+      </Button>
+      <Alert variant="info" className="d-none d-lg-block">
+        Resize your browser to show the responsive offcanvas toggle.
+      </Alert>
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Responsive offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
     </>
   );
 }
-
-export default ResponsiveExample;
