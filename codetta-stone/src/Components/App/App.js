7c401c621922cf4279e33ce4bc7aa3b485a91ba7
Diana <dpoldver@gmail.com>
2023-04-27 11:18:51 +0100

    the Quiz text is now centered on the right of the open sidebar and below the navbar

--- codetta-stone/src/Components/App/App.js
+++ codetta-stone/src/Components/App/App.js
@@ -1,34 +1,41 @@
-
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Sidebar from '../SideBar/index.js';
-import {Col, Row, Container} from 'react-bootstrap';
-import Quiz from '../Quiz/index.js';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Sidebar from "../SideBar/index.js";
+import { Col, Row, Container } from "react-bootstrap";
+import Quiz from "../Quiz/index.js";
 import NavScrollExample from "../Navbar";
-import {BrowserRouter as Router,Routes, Route } from 'react-router-dom';
-import Login from '../Login/index.js';
-import Register from '../Register/index.js';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../Login/index.js";
+import Register from "../Register/index.js";
 
 function App() {
   return (
     <>
-    
-   {/* Navigation Bar and routes to components paths */}
-    <NavScrollExample/> 
-    <Sidebar/> 
-    <Quiz/>
-    
-    <Routes>
-        <Route path="/login" element={<Login />}>
-        </Route>
-        <Route path="/register" element={<Register />}>
-        </Route>
+      {/* Navigation Bar */}
+      <NavScrollExample />
+
+      {/* Main Content */}
+      <div className="container-fluid quiz-container">
+        <Row>
+          {/* Sidebar */}
+          <Col md={3}>
+            <Sidebar />
+          </Col>
+
+          {/* Quiz Questions */}
+          <Col md={9}>
+            <Quiz />
+          </Col>
+        </Row>
+      </div>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
-     
     </>
-  )
-     
+  );
 }
 
 export default App;
